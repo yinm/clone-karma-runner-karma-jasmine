7a9bf4152ce013562ae5c8b2294b53a416c73169
yinm <yinm.cminor9@gmail.com>
2018-04-27 23:43:41 +0900

    Add getRelevantStackFrom()

--- src/adapter.js
+++ src/adapter.js
@@ -4,7 +4,7 @@
 
 /**
  * Decision maker for whether a stack entry is considered external to jasmine and karma.
- * @param {string} entry Error stack entry.
+ * @param {String} entry Error stack entry.
  * @returns {boolean} True if external, False otherwise.
  */
 function isExternalStackEntry (entry) {
@@ -13,4 +13,36 @@ function isExternalStackEntry (entry) {
     !/\/(jasmine-core|karma-jasmine)\//.test(entry) &&
     // karma specifics, e.g. "at http://localhost:7018/karma.js:185"
     !/\/(karma.js|context.html):/.test(entry)
+}
+
+/**
+ * Returns relevant stack entries.
+ * @param {String} stack Complete error stack trace.
+ * @returns {Array} A list of relevant stack entries.
+ */
+function getRelevantStackFrom (stack) {
+  let filteredStack = []
+  let relevantStack = []
+
+  stack = stack.split('\n')
+
+  for (let i = 0; i < stack.length; i += 1) {
+    if (isExternalStackEntry(stack[i])) {
+      filteredStack.push(stack[i])
+    }
+  }
+
+  // If the filtered stack is empty, i.e. the error originated entirely from within jasmine or karma, then the the whole stack
+  // should be relevant.
+  if (filteredStack.length === 0) {
+    filteredStack = stack
+  }
+
+  for (let i = 0; i < filteredStack.length; i += 1) {
+    if (filteredStack[i]) {
+      relevantStack.push(filteredStack[i])
+    }
+  }
+
+  return relevantStack
 }
